e495511ac3917d0c54c2e8a22cbe4e88c4ff9f8c
lingg <kanladaporn.sir@gmail.com>
2024-03-14 13:31:06 +0700

    Fixing bugs in CreateUserServlet

--- src/main/java/io/muzoo/ssc/webapp/servlet/CreateUserServlet.java
+++ src/main/java/io/muzoo/ssc/webapp/servlet/CreateUserServlet.java
@@ -49,67 +49,59 @@ public class CreateUserServlet extends HttpServlet implements Routable {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        boolean authorized = securityService.isAuthorized(request);
-        if (authorized) {
-            String username = StringUtils.trim((String) request.getParameter("username"));
-            String displayName = StringUtils.trim((String) request.getParameter("displayName"));
-            String password = (String) request.getParameter("password");
-            String cpassword = (String) request.getParameter("cpassword");
+        String username = StringUtils.trim((String) request.getParameter("username"));
+        String displayName = StringUtils.trim((String) request.getParameter("displayName"));
+        String password = (String) request.getParameter("password");
+        String cpassword = (String) request.getParameter("confirm-password");
 
-            UserService userService = UserService.getInstance();
-            String errorMessage = null;
+        UserService userService = UserService.getInstance();
+        String errorMessage = null;
 
-            System.out.println("Creating user");
-            // Check if username is valid
-            if (userService.findByUsername(username) != null) {
-                errorMessage = String.format("Username %s has already been taken.", username);
-            }
-
-            // Username must start with 'u'
-            else if (!username.startsWith("u")) {
-                errorMessage = "Username must start with the letter 'u'.";
-            }
-
-            // Confirm password match
-            else if (!StringUtils.equals(password,cpassword)) {
-                errorMessage = "Confirmed password mismatches";
-            }
-
-            if(errorMessage != null){
-                request.getSession().setAttribute("hasError", true);
-                request.getSession().setAttribute("message", errorMessage);
-
-            } else {
-                // Create a user
-                try{
-                    // If no error redirect
-                    userService.createUser(username, password, displayName);
-                    System.out.println("User created");
-                    request.getSession().setAttribute("hasError", false);
-                    request.getSession().setAttribute("message", String.format("User %s has been created successfully", username));
-                    response.sendRedirect("/");
-
-                } catch (Exception e){
-                    request.getSession().setAttribute("hasError", true);
-                    request.getSession().setAttribute("message", e.getMessage());
-
-                }
-            }
-            request.setAttribute("username",username);
-            request.setAttribute("displayName",displayName);
-            request.setAttribute("password",password);
-            request.setAttribute("cpassword",cpassword);
-
-            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/login.jsp");
-            rd.include(request, response);
-
-            request.getSession().removeAttribute("hasError");
-            request.getSession().removeAttribute("message");
-        } else {
-            request.removeAttribute("hasError");
-            request.removeAttribute("message");
-            response.sendRedirect("/login");
+        // Check if username is valid
+        if (userService.findByUsername(username) != null) {
+            errorMessage = String.format("Username %s has already been taken.", username);
         }
+
+        // Username must start with 'u'
+        else if (!username.startsWith("u")) {
+            errorMessage = "Username must start with the letter 'u'.";
+        }
+
+        // Confirm password match
+        else if (!StringUtils.equals(password,cpassword)) {
+            errorMessage = "Confirmed password mismatches";
+        }
+
+        if(errorMessage != null){
+            request.getSession().setAttribute("hasError", true);
+            request.getSession().setAttribute("message", errorMessage);
+
+        } else {
+            // Create a user
+            try{
+                // If no error redirect
+                userService.createUser(username, password, displayName);
+                System.out.println("User created");
+                request.getSession().setAttribute("hasError", false);
+                request.getSession().setAttribute("message", String.format("User %s has been created successfully", username));
+                response.sendRedirect("/");
+
+            } catch (Exception e){
+                request.getSession().setAttribute("hasError", true);
+                request.getSession().setAttribute("message", e.getMessage());
+
+            }
+        }
+        request.setAttribute("username",username);
+        request.setAttribute("displayName",displayName);
+        request.setAttribute("password",password);
+        request.setAttribute("cpassword",cpassword);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/login.jsp");
+        rd.include(request, response);
+
+        request.getSession().removeAttribute("hasError");
+        request.getSession().removeAttribute("message");
     }
 }
 
