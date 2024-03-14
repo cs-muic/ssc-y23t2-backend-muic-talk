e4d1545d18243e118aca78e7cac667100ef69ee3
6480689 <lasee.len@student.mahidol.edu>
2024-03-15 01:42:27 +0700

    Fix login error when another user is already login

--- src/main/java/io/muzoo/ssc/project/backend/auth/AuthenticationController.java
+++ src/main/java/io/muzoo/ssc/project/backend/auth/AuthenticationController.java
@@ -3,6 +3,7 @@ package io.muzoo.ssc.project.backend.auth;
 import io.muzoo.ssc.project.backend.SimpleResponseDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,12 @@ public class AuthenticationController {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         try{
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if(principal != null && principal instanceof org.springframework.security.core.userdetails.User){
+                request.logout();
+            }
+
+
             request.login(username, password);
             return SimpleResponseDTO
                     .builder()
@@ -51,7 +58,7 @@ public class AuthenticationController {
             return SimpleResponseDTO
                     .builder()
                     .success(false)
-                    .message("failed to logout")
+                    .message(e.getMessage())
                     .build();
         }
     }
