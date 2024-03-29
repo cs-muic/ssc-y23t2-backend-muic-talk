6a10b671e6ab0dda97d3c26248518812e4ae49eb
Phavarisa Limchitti <aom.limchitti@gmail.com>
2024-03-29 21:26:04 +0700

    Create UserController

---
+++ src/main/java/io/muzoo/ssc/project/backend/user/UserController.java
@@ -0,0 +1,36 @@
+package io.muzoo.ssc.project.backend.user;
+
+import io.muzoo.ssc.project.backend.User;
+import io.muzoo.ssc.project.backend.UserRepository;
+import io.muzoo.ssc.project.backend.SimpleResponseDTO;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+// You don't have to create a UserController or UserService object explicitly,
+// Spring will do for youuu
+@RestController
+public class UserController {
+
+    // Overusing Autowiring or injections may create cyclic dependency
+    // e.g. class A requires class B but class B also requires class A (loopy)
+    @Autowired // Spring wires it for you (I think that means it will create object?)
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @PostMapping("/user/create")
+    public void createUser(@RequestParam String username,
+                             @RequestParam String name,
+                             @RequestParam String password) {
+        System.out.println("hello");
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setName(name);
+        newUser.setPassword(passwordEncoder.encode(password));
+        newUser.setRole("USER");
+        userRepository.save(newUser);
+        System.out.println("Success");
+    }
+}
