f3ea6bff1f521ef02d67d5cfd35a80fbb8b4aee0
lingg <kanladaporn.sir@gmail.com>
2024-04-03 17:47:42 +0700

    Event Class for storing events

---
+++ src/main/java/io/muzoo/ssc/project/backend/user/Event.java
@@ -0,0 +1,44 @@
+package io.muzoo.ssc.project.backend.user;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private int id;
+    private String name;
+    private LocalDateTime dateTime;
+
+    public Event(int id, String name, LocalDateTime dateTime) {
+        this.id = id;
+        this.name = name;
+        this.dateTime = dateTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDateTimeString() {
+        return dateTime.toString();
+    }
+
+}
