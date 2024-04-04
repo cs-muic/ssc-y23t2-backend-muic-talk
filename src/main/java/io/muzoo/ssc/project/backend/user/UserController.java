a6305ab0447c17cba11923b0005a3ecd825706ab
Phavarisa Limchitti <aom.limchitti@gmail.com>
2024-04-05 00:49:12 +0700

    Update deletion in other tables when deleting user

--- src/main/java/io/muzoo/ssc/project/backend/user/UserController.java
+++ src/main/java/io/muzoo/ssc/project/backend/user/UserController.java
@@ -1,8 +1,15 @@
 package io.muzoo.ssc.project.backend.user;
 
 import io.muzoo.ssc.project.backend.SimpleResponseDTO;
+import io.muzoo.ssc.project.backend.chat.Chat;
+import io.muzoo.ssc.project.backend.chat.ChatRepository;
 import io.muzoo.ssc.project.backend.friend.Friend;
 import io.muzoo.ssc.project.backend.friend.FriendRepository;
+import io.muzoo.ssc.project.backend.group.Group;
+import io.muzoo.ssc.project.backend.group.GroupRepository;
+import io.muzoo.ssc.project.backend.schedule.Event;
+import io.muzoo.ssc.project.backend.schedule.EventController;
+import io.muzoo.ssc.project.backend.schedule.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
+import java.util.Set;
 
 // You don't have to create a UserController or UserService object explicitly,
 // Spring will do for youuu
@@ -26,6 +34,15 @@ public class UserController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
 
     @PostMapping("/user/create")
     public SimpleResponseDTO createUser(@RequestParam String username,
@@ -115,8 +132,21 @@ public class UserController {
                                             @RequestParam String password) {
         User user = userRepository.findFirstByUsername(username);
         List<Friend> friendList = friendRepository.findAllByUser1OrUser2(user, user);
+        List<Event> events = eventRepository.findAllByUser(user);
+        List<Chat> chats = chatRepository.findAllBySender(user);
+        Set<Group> groups = user.getGroups();
         if (BCrypt.checkpw(password, user.getPassword())) {
             friendRepository.deleteAll(friendList);
+            eventRepository.deleteAll(events);
+            User nullUser = userRepository.findFirstByUsername("null");
+            for (Chat chat : chats) {
+                chat.setSender(nullUser);
+            }
+            chatRepository.saveAll(chats);
+            for (Group group : groups) {
+                group.getUsers().remove(user);
+            }
+            groupRepository.saveAll(groups);
             userRepository.delete(user);
             return SimpleResponseDTO
                     .builder()
